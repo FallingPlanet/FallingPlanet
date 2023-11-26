7fb37bd1e2e29833694a89b83c8edca65a92bdba
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2023-11-26 00:56:00 -0500

    Prepared files for package publishing

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt and store its contents in a list
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='FallingPlanet',
+    version='0.1',
+    packages=find_packages(),
+    description='A PyTorch-based toolkit for machine learning and NLP tasks',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='William Stigall',
+    author_email='fallingplanetcorp@gmail.com',
+    url='https://github.com/FallingPlanet/FallingPlanet',
+    install_requires=required,
+    python_requires='>=3.6',
+)
