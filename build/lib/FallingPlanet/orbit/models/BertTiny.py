eeee0a49c1cb03affc43f2025022b430d1da2250
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2023-11-26 02:59:18 -0500

    Fixed Everything and Deployed

---
+++ build/lib/FallingPlanet/orbit/models/BertTiny.py
@@ -0,0 +1,24 @@
+from transformers import BertModel
+import torch.nn as nn
+import torch
+class BertFineTune(nn.Module):
+    def __init__(self, num_labels, from_saved_weights=None, num_tasks=1, **kwargs):
+        super(BertFineTune, self).__init__()
+        self.bert = BertModel.from_pretrained("prajjwal1/bert-tiny")
+        
+        if from_saved_weights:
+            self.bert.load_state_dict(torch.load(from_saved_weights))
+
+        if num_tasks > 1:
+            self.classifiers = nn.ModuleList([nn.Linear(128, n_labels) for n_labels in num_labels])
+        else:
+            self.classifier = nn.Linear(128, num_labels[0])
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output = outputs.pooler_output
+
+        if hasattr(self, 'classifiers'):
+            return [classifier(pooled_output) for classifier in self.classifiers]
+        else:
+            return self.classifier(pooled_output)
---
+++ build/lib/FallingPlanet/orbit/utils/Tokenizer.py
@@ -0,0 +1,35 @@
+import transformers
+import torch
+from transformers import BertTokenizerFast
+
+def BertTiny_tokenize(text, max_length=256):
+    tokenizer = BertTokenizerFast.from_pretrained("prajjwal1/bert-tiny")
+    encoded = tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=max_length,
+            truncation=True,  # Explicitly activate truncation
+            padding='max_length',  # Pad to max_length
+            return_attention_mask=True,
+            return_tensors="pt"
+    )
+    
+    return encoded["input_ids"], encoded["attention_mask"]
+
+
+
+def BertTiny_batch_tokenize(texts, max_length=256):
+    tokenizer = BertTokenizerFast.from_pretrained("prajjwal1/bert-tiny")
+    encoded_batch = tokenizer.batch_encode_plus(
+        texts,
+        add_special_tokens=True,
+        max_length=max_length,
+        truncation=True,
+        padding='max_length',
+        return_attention_mask=True,
+        return_tensors="pt"
+    )
+    
+    return encoded_batch["input_ids"], encoded_batch["attention_mask"]
+
+
