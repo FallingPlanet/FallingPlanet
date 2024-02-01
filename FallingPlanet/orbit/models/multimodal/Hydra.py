709c2859d878b1e75e09b43faf465a605ac94ae6
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2024-02-01 13:14:05 -0500

    Separated Classes

--- FallingPlanet/orbit/models/multimodal/Hydra.py
+++ FallingPlanet/orbit/models/multimodal/Hydra.py
@@ -8,19 +8,5 @@ class HydraTiny(nn.Module):
         super().__init__()
         
         
-class Chimera(nn.Module):
-    def __init__(self,text_model, vision_model, audio_model):
-        super(Chimera, self).__init__()
-        self.text_model = text_model
-        self.vision_model = vision_model
-        self.audio_model = audio_model
-        
-    def forward(self, text_input, vision_input, audio_input):
-        text_output = self.text_model(text_input)
-        vision_output = self.vision_model(vision_input)
-        audio_output = self.audio_model(vision_output)
-        
-        combined_output = torch.cat([text_output,vision_output,audio_output])
-        
-        return combined_output
+
     
