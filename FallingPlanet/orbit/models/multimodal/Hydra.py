cc32c4c6641357f2636a02f59335be29469bdb08
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2024-02-06 14:31:52 -0500

    Define fusion model (WIP)

--- FallingPlanet/orbit/models/multimodal/Hydra.py
+++ FallingPlanet/orbit/models/multimodal/Hydra.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
-from transformers import ViTModel, ViTFeatureExtractor
+from transformers import ViTModel, ViTFeatureExtractor, DeiTForImageClassification
+from AuTransformer import FPATF_Tiny
+from BertFineTuneForSequenceClassification import BertFineTuneTiny
 
 class HydraTiny(nn.Module):
     def __init__(self, num_classes, vision_model_name, text_model_config, audio_model_config):
@@ -8,7 +10,7 @@ class HydraTiny(nn.Module):
         
         # Assuming the vision model is a Vision Transformer
         self.vision_model = ViTModel.from_pretrained(vision_model_name)
-        self.vision_feature_extractor = ViTFeatureExtractor.from_pretrained(vision_model_name)
+        
         
         # Text model placeholder initialization (adjust with actual model)
         self.text_model = ...  # Initialize with text_model_config
@@ -16,15 +18,25 @@ class HydraTiny(nn.Module):
         # Audio model (FPATF_Tiny) initialization
         self.audio_model = FPATF_Tiny(**audio_model_config)
         
+        
         # Fusion mechanism (adjust dimensions as needed)
-        self.fusion_layer = nn.Linear(..., num_classes)  # Update based on your fusion strategy
+        self.fusion_layer = nn.Linear(128+128+256, num_classes)  # Update based on your fusion strategy
+        f_transformer_layer_1 = nn.TransformerEncoderLayer(d_model = 512,nhead=8, dim_feedforward=1024, dropout = .01)
+        self.f_transformer_1 = nn.TransformerEncoder(f_transformer_layer_1, num_layers = 6)
+        self.f_layer_norm = nn.layer_norm = nn.LayerNorm(512)
+        self.f_fc = nn.Linear(512,256)
+        f_transformer_layer_2 = nn.TransformerEncoderLayer(d_model = 256, nhead=4, dim_feedforward = 512, dropout = .01)
+        self.f_transformer_2 = nn.TransformerEncoder(f_transformer_layer_2, num_layers = 2)
+        self.f_fc2 = nn.Linear(256,128)
+        self.f_fc3 = nn.Linear(128, num_classes)
+
         
         self.num_classes = num_classes
 
     def forward(self, vision_inputs, text_input, audio_input):
         # Process vision inputs (a list of frames)
-        vision_features = [self.vision_feature_extractor(images=frame, return_tensors="pt")['pixel_values'] for frame in vision_inputs]
-        vision_logits_list = [self.vision_model(pixel_values=features).logits for features in vision_features]
+        
+        vision_logits_list = [self.vision_model(pixel_values=features).logits for features in vision_inputs]
         
         # Average the logits from the vision model frames
         vision_logits = torch.mean(torch.stack(vision_logits_list), dim=0)
